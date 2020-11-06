e6c63ed100836b07c3be97d8118411619ae90f14
Megan Sleezer <64713309+mslee001@users.noreply.github.com>
2020-11-05 22:13:07 -0800

    updated model path and added new route to display the last workout

---
+++ routes/exerciseRoutes.js
@@ -0,0 +1,57 @@
+const router = require("express").Router();
+// const path = require('path');
+const Workout = require("../models/Workout.js");
+
+router.get("/exercise", (req,res) => {
+    res.sendFile("exercise.html", { root: "public" })
+});
+
+router.get("/stats", (req,res) => {
+    res.sendFile("stats.html", { root: "public" })
+});
+
+router.get("/api/workouts/range", (req,res) => {
+    Workout.find({})
+    .sort({date: -1})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts", (req,res) => {
+    Workout.find({})
+    .sort({ date: -1 })
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+    console.log(req.params.id);
+    console.log(req.body)
+    Workout.findOneAndUpdate({_id: req.params.id}, { $push: { exercises: req.body } }, { new: true })
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+module.exports = router;
