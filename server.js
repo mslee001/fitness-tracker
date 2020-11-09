c4c39a732483aa8cc9980b4d58521507dabdbee5
mslee001 <megtej@gmail.com>
2020-11-08 17:32:03 -0800

    updated mongoose connection information

--- server.js
+++ server.js
@@ -13,7 +13,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessTracker", { useNewUrlParser: true,  useFindAndModify: false });
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/fitness-tracker',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.use(require("./routes/exerciseRoutes.js"));
 
